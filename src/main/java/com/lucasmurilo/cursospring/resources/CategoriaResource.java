ebb185bee4e99ce576202275cc2a96c41dd8cce0
LucasMurilo\Lucas Murilo <lucas.oliveira@join4.com.br>
2022-04-19 10:13:17 -0400

    Testando o endpoint

---
+++ src/main/java/com/lucasmurilo/cursospring/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.lucasmurilo.cursospring.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @GetMapping
+    public String testar(){
+        return "Teste validado";
+    }
+}
